8ad3ff9ebe55699b8b180c7de06223f628f37f0a
HaniOdh <mhoudih@gmail.com>
2025-12-28 23:08:33 +0100

    linked with backend and things crash when fetching

--- server/history_api/settings.py
+++ server/history_api/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-i2qkf9_=-8=@6h)vbpeo=c3fth^%tjsiij*p)vidtnbxwzi$n)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '10.0.2.2',
+    '192.168.56.1',
+]
 
 
 # Application definition
